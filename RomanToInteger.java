7342082c835ca9a3e97453fc2b1c978eb04b09a4
justnisar <justnisar@gmail.com>
2016-06-24 13:05:02 -0700

    Addeed Problem #13

---
+++ RomanToInteger.java
@@ -0,0 +1,32 @@
+public class RomanToInteger {
+    
+    HashMap<Character,Integer> m = new HashMap<Character,Integer>();
+        
+        public Solution(){
+            m.put('I',1);
+        m.put('V',5);
+        m.put('X',10);
+        m.put('L',50);
+        m.put('C',100);
+        m.put('D',500);
+        m.put('M',1000);
+        }
+    /*
+    public int getNegative(int x){
+        return -x;
+    }
+    */
+    
+    public int romanToInt(String s) {
+        int result = 0;
+        for(int i = 0 ; i < s.length() - 1 ; i++){
+            if( m.get( s.charAt(i) ) >= m.get( s.charAt(i+1) ) ){
+                result += m.get( s.charAt(i) );
+            }
+            else{
+                result -= m.get( s.charAt(i) );
+            }
+        }
+        return result + m.get(s.charAt(s.length() - 1));
+    }
+}
