e0abc749b38de27ca022368a647630671da8ce8b
justnisar <justnisar@gmail.com>
2016-06-29 14:10:23 -0700

    Added Problem #62

---
+++ UniquePaths.java
@@ -0,0 +1,18 @@
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int [][]a = new int[m][n];
+        int i=0,j=0;
+        for(i = 0 ; i < m ; i++)
+            a[i][0] = 1;
+        for(i = 0 ; i < n ; i++)
+            a[0][i] = 1;
+        
+        for(i = 1 ; i < m ; i++){
+            for(j = 1; j < n ;j++){
+                a[i][j] = a[i-1][j] + a[i][j-1];
+            }
+        }
+        
+        return a[m-1][n-1];
+    }
+}
