cf2c7583a86d8f42b134171c17a55284381c4882
justnisar <justnisar@gmail.com>
2016-06-29 15:48:11 -0700

    Added Problem #75

---
+++ SortColors.java
@@ -0,0 +1,29 @@
+public class SortColors {
+    public void sortColors(int[] nums) {
+        
+        int count[] = new int[3];
+        
+        int n = nums.length;
+        int i = 0;
+        for(i = 0 ; i < n ; i++)
+            count[nums[i]]++;
+            
+        int  x = 0;
+        
+        i = 0;
+        
+        while(i < 3){
+            
+            int j = count[i];
+            
+            while(j-- > 0){
+                nums[x] = i;
+                x++;
+            }
+            i++;
+            
+        }
+        
+        
+    }
+}
