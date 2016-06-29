285f9ac377652011bc1127fd728813fd3f321545
justnisar <justnisar@gmail.com>
2016-06-29 16:06:31 -0700

    Added Problem #102

---
+++ BinaryTreeLevelOrder.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BinaryTreeLevelOrder {
+    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        
+        List<List<Integer>> l = new ArrayList<List<Integer>>();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        if(root != null)
+            q.add(root);
+        q.add(null);
+        while(q.peek() != null){
+            List<Integer> ll = new ArrayList<Integer>();
+            while(true){
+                TreeNode x = q.remove();
+                if(x == null){
+                    q.add(null);        
+                    break;
+                    
+                }
+                ll.add(x.val);
+                if(x.left != null)
+                    q.add(x.left);
+                if(x.right != null)
+                    q.add(x.right);
+            }
+            l.add(ll);
+        }
+    return l;
+        
+    }
+}
