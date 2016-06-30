b3f4240bd17ae09158b25e7b37d85982a397ac7e
justnisar <justnisar@gmail.com>
2016-06-30 13:00:04 -0700

    Added Problem #231

---
+++ PowerOfTwo.java
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        double logar = log2(n);
+        if(logar - (int)logar == 0)
+            return true;
+        return false;
+        
+    }
+};
