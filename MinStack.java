8743390476f576cc1ba0e2ee12857e8cacda4334
justnisar <justnisar@gmail.com>
2016-06-30 11:57:49 -0700

    Added Problem #155

---
+++ MinStack.java
@@ -0,0 +1,43 @@
+class MinStack {
+    Stack<Integer> main_stack;
+    Stack<Integer> min_stack;
+    public MinStack(){
+        main_stack = new Stack<Integer>();
+        min_stack  = new Stack<Integer>();
+    }
+    public void push(int x) {
+        if(main_stack.empty() == true){
+            main_stack.push(x);
+            min_stack.push(x);
+        }
+        else{
+            int min = getMin();
+            if(x <= min){
+                main_stack.push(x);
+                min_stack.push(x);
+            }
+            else
+                main_stack.push(x);
+        }
+    }
+
+    public void pop() {
+        int top_element = top();
+        int min_element = getMin();
+        if(top_element == min_element){
+            main_stack.pop();
+            min_stack.pop();
+        }
+        else
+            main_stack.pop();
+    }
+
+    public int top() {
+        return main_stack.peek();
+    }
+
+    public int getMin() {
+        return min_stack.peek();
+    }
+}
+
