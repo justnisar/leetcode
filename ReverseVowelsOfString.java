d0dd9c11b105933f33142b44044d8096e29baae4
justnisar <justnisar@gmail.com>
2016-06-30 14:23:44 -0700

    Added Problem #345

---
+++ ReverseVowelsOfString.java
@@ -0,0 +1,33 @@
+public class ReverseVowelsOfString {
+    
+    public boolean isVowel(char ch){
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' )
+            return true;
+            return false;
+    }
+    
+    public String reverseVowels(String s) {
+        char[] c = s.toCharArray();
+        int n = c.length;
+        int i = 0;
+        int j = c.length - 1;
+        while(true){
+            
+            while(i < n && !isVowel(c[i]) )
+                ++i;
+            while(j >= 0 && !isVowel(c[j]) )
+                --j;
+            
+            if(i >= j)
+                break;
+                
+            char t = c[i];
+            c[i] = c[j];
+            c[j] = t;
+            ++i;
+            --j;
+        }
+        return new String(c);
+    }
+    
+}
