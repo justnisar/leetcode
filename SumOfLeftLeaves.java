49fb6689573b147e8557613d47ec8c97c50ec4dc
Nisar Shaik (Student) <nshaik2@sundevils.asu.edu>
2016-12-08 17:27:57 -0700

    Added Problem #404

---
+++ SumOfLeftLeaves.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class SumOfLeftLeaves {
+    
+    public boolean isLeafNode(TreeNode root){
+        if(root == null)
+            return false;
+        else if(root.left == null && root.right == null)
+            return true;
+        else
+            return false;
+    }
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        if(root == null){
+            return sum;
+        }
+        if(isLeafNode(root.left)){
+            sum += root.left.val;
+        }
+        else{
+            sum += sumOfLeftLeaves(root.left);
+        }
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
