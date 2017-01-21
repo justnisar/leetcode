9f25d845a1e6df08cbb38f7b59180d5b978867f2
Nisar <justnisar@gmail.com>
2017-01-21 14:32:47 -0800

    Added Problem #442

---
+++ FindAllDuplicatesInAnArray.java
@@ -0,0 +1,35 @@
+public class FindAllDuplicatesInAnArray {
+
+    public List<Integer> findDuplicates(int[] nums) {
+
+        
+
+        int length = nums.length;
+
+        HashSet<Integer> hs = new HashSet<Integer>();
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        for(int i = 0; i < length ; i++){
+
+            if(!hs.contains(nums[i])){
+
+                hs.add(nums[i]);
+
+            }
+
+            else{
+
+                result.add(nums[i]);
+
+            }
+
+        }
+
+        return result;
+
+        
+
+    }
+
+}
