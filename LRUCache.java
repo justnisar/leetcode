e8a88c1c1e3c675dceef3c146fa6950c8f58af30
Nisar Shaik <justnisar@gmail.com>
2017-04-23 14:16:33 -0700

    Added Problem #146

---
+++ LRUCache.java
@@ -0,0 +1,27 @@
+public class LRUCache {
+
+    private LinkedHashMap<Integer,Integer> map;
+	private int capacity;
+	
+	public LRUCache(final int capacity){
+		this.capacity = capacity;
+		map = new LinkedHashMap<Integer,Integer>(capacity,0.75f,true){
+			protected boolean removeEldestEntry(Map.Entry eldest) {
+		        return size() > capacity;
+		     }
+		};
+	}
+	
+	public int get(int key) {
+		if(!map.containsKey(key))
+			return -1;
+		return map.get(key);
+    }
+    
+	
+	
+    public void put(int key, int value) {
+    	map.put(key, value);
+    }
+
+}
