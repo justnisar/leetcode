2c42f3dc857d5ab6458e76dcc0fc1441ce721cdd
Nisar Shaik <justnisar@gmail.com>
2017-05-12 18:07:42 -0700

    Added Problem #504

---
+++ Base7.java
@@ -0,0 +1,19 @@
+
+public class Base7 {
+    public String convertToBase7(int num) {
+    if(num == 0)
+      return "0";
+    StringBuffer result = new StringBuffer();
+    int n = num;
+    if(num < 0)
+      num = Math.abs(num);
+    while(num != 0){
+      int reminder = num % 7;
+      result.append((char)(reminder + 48));
+      num = num/7;
+    }
+    if(n < 0)
+      result.append('-');
+    return result.reverse().toString();
+  }
+}
