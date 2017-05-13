fa1ff47fecba71132e5a030570e01d2c84120874
Nisar Shaik <justnisar@gmail.com>
2017-05-12 18:55:42 -0700

    Added Problem #543

---
+++ DiameterOfABinaryTree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class DiameterOfABinaryTree {
+    int max;
+  
+  public Solution(){
+    max = 0;
+  }
+  
+  public int recfun(TreeNode root){
+    if(root == null)
+      return 0;
+    int leftHeight = recfun(root.left);
+    int rightHeight = recfun(root.right);
+    if(leftHeight + rightHeight > max)
+        max = leftHeight + rightHeight;
+    if(leftHeight > rightHeight)
+        return 1 + leftHeight;
+    return 1 + rightHeight;
+  }
+  
+  public int diameterOfBinaryTree(TreeNode root) {
+    recfun(root);
+    return max;
+  }
+}
