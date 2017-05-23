dd3a89dc26bd5759ef20e6c34f9cbc33ade6ffb3
Nisar Shaik <justnisar@gmail.com>
2017-05-23 15:31:12 -0700

    Added Problem #532

---
+++ KDiffPairsInAnArray.java
@@ -0,0 +1,24 @@
+public class KDiffPairsInAnArray {
+    public int findPairs(int[] nums, int k) {
+        if(k < 0)
+            return 0;
+        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        int length = nums.length;
+        int result = 0;
+        for(int i = 0 ; i < length ; i++)
+          hm.put(nums[i],hm.getOrDefault(nums[i],0) + 1);
+        for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
+          if(k == 0){
+              if(entry.getValue() >= 2)
+                result++;
+            
+          }
+          else{
+              if(hm.containsKey(k + entry.getKey()))
+                result++;
+          }
+          
+        }
+        return result;
+    }
+}
