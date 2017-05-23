34c80d5da47f60d01943464319fab23b45c4c8f5
Nisar Shaik <justnisar@gmail.com>
2017-05-23 15:28:07 -0700

    Added Problem #476

---
+++ NumberComplement.java
@@ -0,0 +1,12 @@
+public class NumberComplement {
+    public int findComplement(int num) {
+        int number = 0;
+        int count = 0;
+        while(number < num){
+            number = number + (int)Math.pow(2,count);
+            count++;
+        }
+        return number - num;
+        
+    }
+}
