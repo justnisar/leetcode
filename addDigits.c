7686ff60a9b8dc066cb3c7680345021190416845
Nisar Shaik <justnisar@gmail.com>
2017-06-14 13:56:02 -0700

    Added Problem #258 C

---
+++ addDigits.c
@@ -0,0 +1,19 @@
+int getsum(int num){
+        int sum = 0;
+        while(num != 0){
+            sum = sum + num % 10;
+            num = num/10;
+        }
+        return sum;
+    }
+
+int addDigits(int num) {
+    int sum = 0;
+    while(num != 0){
+    int n = getsum(num);
+    if(n < 10)
+        return n;
+    num = n;
+}
+return 0;
+}
