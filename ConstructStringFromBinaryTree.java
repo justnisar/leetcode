36e63282822c5bda36d0560bce853a4da31ff231
Nisar Shaik <justnisar@gmail.com>
2017-07-01 20:51:34 -0700

    Added Problem #606

---
+++ ConstructStringFromBinaryTree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class ConstructStringFromBinaryTree {
+    public String tree2str(TreeNode t) {
+        if(t == null)
+      return "";
+    String result = t.val + "";
+    String leftString = tree2str(t.left);
+    String rightString = tree2str(t.right);
+    if(leftString == "" && rightString == "")
+      return result;
+    if(leftString == "")
+      return result + "()" + "(" + rightString + ")";
+    if(rightString == "")
+      return result + "(" + leftString + ")";
+    return result + "(" + leftString + ")" + "(" + rightString + ")";
+    }
+}
