0a40221f120c8cc6ae0d9638d178f8bcdc2c3641
Nisar Shaik <justnisar@gmail.com>
2017-07-01 22:27:19 -0700

    Added Problem #501

---
+++ FindModeInBinarySearchTree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+   private List<Integer> result;
+     private int maxCount;
+    private TreeNode prev;
+    private int count;
+  public Solution(){
+    result = new ArrayList<Integer>();
+    maxCount = 0;
+      prev = null;
+      count = 0;
+  }
+  
+  public void recFun(TreeNode root){
+    if(root == null)
+      return;
+    
+    recFun(root.left);
+    
+    if(prev == null){
+      count = 1;
+    }
+    else if(root.val == prev.val){
+      count++;
+    }
+    else{
+      count = 1;
+    }
+    
+    if(count > maxCount){
+      result.clear();
+      result.add(root.val);
+      maxCount = count;
+    }
+    else if(count == maxCount){
+      result.add(root.val);
+    }
+    
+    prev = root;
+    recFun(root.right);
+  }
+  
+  public int[] findMode(TreeNode root) {
+    recFun(root);
+    return result.stream().mapToInt((Integer i)->i).toArray();
+  }
+}
