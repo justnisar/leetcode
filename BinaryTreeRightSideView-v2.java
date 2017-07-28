8fba0dec6298f219a39505edc718029041bb1a95
Nisar Shaik <justnisar@gmail.com>
2017-07-28 00:56:52 -0700

    Added Problem # 199

---
+++ BinaryTreeRightSideView-v2.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BinaryTreeRightSideView-v2 {
+    
+    int max;
+    List<Integer> result;
+    public Solution(){
+        result = new ArrayList<Integer>();
+        max = -1;
+    }
+    
+    public void recfun(TreeNode root,int currentLevel){
+        if(root == null)
+            return;
+        if(currentLevel > max){
+            result.add(root.val);
+            max = currentLevel;
+        }
+        recfun(root.right,currentLevel + 1);
+        recfun(root.left,currentLevel + 1);
+    }
+    
+    public List<Integer> rightSideView(TreeNode root) {
+        recfun(root,0);
+        return result;
+    }
+}
